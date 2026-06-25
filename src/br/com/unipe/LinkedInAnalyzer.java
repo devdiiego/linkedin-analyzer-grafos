3058979afe0cecc7de5998caf078b193ffb09840
devdiiego <diegofeitoza2002@gmail.com>
2026-06-25 19:54:56 -0300

    Implementação da Missão 2: Sugestão de conexões por amigos em comum

--- src/br/com/unipe/LinkedInAnalyzer.java
+++ src/br/com/unipe/LinkedInAnalyzer.java
@@ -1,18 +1,48 @@
 package br.com.unipe;
 
 import java.util.*;
+import br.com.unipe.Grafo.Vertice;
+import br.com.unipe.Grafo.Aresta;
 
 public class LinkedInAnalyzer {
     private final Grafo grafo;
 
-    // Missão 1: Construtor
+    /// Missão 1: Construtor principal
     public LinkedInAnalyzer(Grafo grafo) {
         this.grafo = grafo;
     }
 
-    // Missão 2: Sugestão de Conexões
+    // Missão 2: Sugestão de Conexões (Diego.F)
     public List<Map.Entry<String, Integer>> sugerirConexoes(String nomePessoa) {
-        return Collections.emptyList();
+        Optional<Vertice> alvoOpt = grafo.encontraVertice(nomePessoa);
+        if (alvoOpt.isEmpty()) return Collections.emptyList();
+
+        Vertice alvo = alvoOpt.get();
+        Set<Vertice> contatosDiretos = new HashSet<>();
+        for (Aresta a : alvo.getArestas()) {
+            contatosDiretos.add(a.getDestino());
+        }
+
+        Map<Vertice, Integer> contagemAmigosEmComum = new HashMap<>();
+
+        for (Vertice amigo : contatosDiretos) {
+            for (Aresta a : amigo.getArestas()) {
+                Vertice amigoDoAmigo = a.getDestino();
+
+                if (!amigoDoAmigo.equals(alvo) && !contatosDiretos.contains(amigoDoAmigo)) {
+                    contagemAmigosEmComum.put(amigoDoAmigo, contagemAmigosEmComum.getOrDefault(amigoDoAmigo, 0) + 1);
+                }
+            }
+        }
+
+        List<Map.Entry<String, Integer>> resultado = new ArrayList<>();
+        for (Map.Entry<Vertice, Integer> entry : contagemAmigosEmComum.entrySet()) {
+            resultado.add(new AbstractMap.SimpleEntry<>(entry.getKey().getNome(), entry.getValue()));
+        }
+
+        resultado.sort((e1, e2) -> e2.getValue().compareTo(e1.getValue()));
+
+        return resultado;
     }
 
     // Missão 3: Grau de Separação
